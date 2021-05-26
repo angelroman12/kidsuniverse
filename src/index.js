f240a9856dac324edd8e238ddaa310c392717083
“Andrei <romanangel95@yahoo.com>
2021-05-26 13:23:26 +0300

    fix mobile

--- src/index.js
+++ src/index.js
@@ -31,3 +31,33 @@ const allProducts = document.getElementById('allProducts').addEventListener("cli
       .then((data) => ui.productsInIndex(data));
 })
 
+
+// Mobile 
+const mobilierMobile = document.getElementById('mobilierMobile');
+mobilierMobile.addEventListener('click', getMobilierMobile);
+function getMobilierMobile () {
+  output.innerHTML = "";
+  http.get("https://60ae021e80a61f00173322ee.mockapi.io/mobilier")
+      .then((data) => ui.productsInIndex(data));
+}
+
+
+const bicicleteMobile = document.getElementById('bicicleteMobile')
+bicicleteMobile.addEventListener('click', getBicicleteMobile);
+function getBicicleteMobile(){
+  output.innerHTML = "";
+  http.get("https://60ae033e80a61f0017332321.mockapi.io/biciclete")
+      .then((data) => ui.productsInIndex(data));
+}
+
+document.getElementById('boardgamesMobile').addEventListener('click', () => {
+  output.innerHTML = "";
+  http.get("https://60ae00fc80a61f00173322ab.mockapi.io/boardGames")
+      .then((data) => ui.productsInIndex(data));
+})
+
+const allProductsMobile = document.getElementById('allProductsMobile').addEventListener("click", () =>{
+  output.innerHTML = "";
+  http.get("https://60adff9080a61f0017332272.mockapi.io/allProducts")
+      .then((data) => ui.productsInIndex(data));
+})
